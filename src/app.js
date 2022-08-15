8ca8670d99b4a03093033161b4e1d3743c3300fd
Delfin656 <juandelfin656@gmail.com>
2022-08-15 19:00:24 +0000

    Excuse Generator JS

--- src/app.js
+++ src/app.js
@@ -8,4 +8,26 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  const h1 = document.querySelector("#excuse");
+
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function excuseGenerator(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  h1.innerHTML =
+    excuseGenerator(who) +
+    " " +
+    excuseGenerator(what) +
+    " " +
+    excuseGenerator(when);
 };
